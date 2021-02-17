a1a0b56030e9cb7467862fde3d2020f22d82f850
kostas xalkiadakis <xalkiadakis1994@gmail.com>
2021-02-17 23:21:03 +0200

    Update DataDaoImpl.java

--- src/dataAcessObject/DataDaoImpl.java
+++ src/dataAcessObject/DataDaoImpl.java
@@ -130,7 +130,8 @@ public class DataDaoImpl implements DataDao {
             System.out.println("Error:" + e.toString() + e.getMessage());
         }
 
-        //return player list with data 
+        //retur list with data 
+
         return dataList;
     }
 
